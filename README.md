f8e75f5302e5ec45f728774a8dddc437a7078da3
CaiCharles01 <150222604+CaiCharles01@users.noreply.github.com>
2024-01-22 13:09:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fd-message-strip-altert-test
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/CaiCharles01/fd-message-strip-altert-test)
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { ApplicationRef } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { RtlService } from '@fundamental-ngx/cdk/utils';
+import { ThemingService, provideTheming } from '@fundamental-ngx/core/theming';
+import { provideDialogService } from '@fundamental-ngx/core/dialog';
+import { MessageStripAlertExampleComponent } from './app/message-strip-alert-example.component';
+
+bootstrapApplication(MessageStripAlertExampleComponent, {
+  providers: [
+    provideAnimations(),
+    provideRouter([]),
+    RtlService,
+    provideTheming({
+      defaultTheme: 'sap_horizon',
+    }),
+    provideDialogService(),
+  ],
+}).then((appRef: ApplicationRef) => appRef.injector.get(ThemingService).init());
